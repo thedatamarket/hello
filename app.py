8dc9a5f439199412d047b1d5823c6d5654ff50e6
The Data Market <110961258+thedatamarket@users.noreply.github.com>
2022-09-29 12:29:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+import dropbox
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    dropbox_access_token= "NVwGrPvFLxgAAAAAAAAAAVUBiyewqJ5KTDlXkSBRNBBsH2-aZ9iKQvRkP1bIDy_G"    #Enter your own access token
+    client = dropbox.Dropbox(dropbox_access_token)
+    print("[SUCCESS] dropbox account linked")
+
+    client.files_delete("/TEST/Sep-24-2022.csv")
     return 'Hello, World!'
 
 if __name__ == "__main__":
